6c63505a2ac78a4e53297c51899d9787c6dd6012
Jeevanandam M <jeeva@myjeeva.com>
2018-08-17 09:21:56 -0700

    enabled let's encrypt cert and configs

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,23 +1,22 @@
 package controllers
 
 import (
-  "aahframework.org/aah.v0"
-
-  "gorepositree.com/app/models"
+	"aahframework.org/aah.v0"
 )
 
 // AppController struct application controller
 type AppController struct {
-  *aah.Context
+	*aah.Context
 }
 
 // Index method is application's home page.
 func (c *AppController) Index() {
-  data := aah.Data{
-    "Greet": models.Greet{
-      Message: "Welcome to aah framework - Web Application",
-    },
-  }
+	c.Reply().Text("Coming soon...")
+	// data := aah.Data{
+	//   "Greet": models.Greet{
+	//     Message: "Welcome to aah framework - Web Application",
+	//   },
+	// }
 
-  c.Reply().Ok().HTML(data)
+	// c.Reply().Ok().HTML(data)
 }
