635f2a0ec6ea132b2093f09b4b73ff28e02ac7d1
Jeevanandam M <jeeva@myjeeva.com>
2018-09-12 22:32:18 -0700

    header update with year

--- app/controllers/admin/vanity_controller.go
+++ app/controllers/admin/vanity_controller.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Jeevanandam M. (https://github.com/jeevatkm, jeeva@myjeeva.com)
+// Copyright Jeevanandam M. (https://github.com/jeevatkm, jeeva@myjeeva.com)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
